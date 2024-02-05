0669528bc5df4bf6420c9e902e33beecd285f76a
Roman Korsun <4517566+bo-bac@users.noreply.github.com>
2024-02-05 12:55:50 +0200

    fix layout

--- src/Tyts.BLambda.Shell/Program.cs
+++ src/Tyts.BLambda.Shell/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.Logging;
 using Microsoft.FluentUI.AspNetCore.Components;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using Tyts.BLambda.Blazor.Components;
 using Tyts.BLambda.Shell;
 
 Debug.Assert(OperatingSystem.IsBrowser());
@@ -13,16 +15,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// ???
 var http = new HttpClient()
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 };
 builder.Services.AddSingleton(sp => http);
 
-builder.Services.AddCascadingAuthenticationState();
 
-//builder.Services.AddFluentUIComponents();
 await builder.AddShell();
 
 var app = builder.Build();
+app.UseControls();
+
 await app.RunAsync();
